0bb96aa3a86605b2cf9f553c91126fdee9ef5876
abdulsalam aisha <abdulsalamaisha45@gmail.com>
2026-02-04 13:25:24 +0100

    third commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,1 +1,13 @@
- 
+from django.db import models
+from django.contrib.auth.models import User
+
+class Calculation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    num1 = models.FloatField()
+    num2 = models.FloatField()
+    operator = models.CharField(max_length=1)
+    result = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.num1} {self.operator} {self.num2} = {self.result}"
